3388e7d7e1df6b6b2863cba07d5f766b65c9d269
Jonathan <jonathan.vengadasalam@gmail.com>
2019-09-23 22:52:18 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,38 @@
-def merge_n_diff(p1, p2, pk, flt_col, str_col, how="outer"):
+# -*- coding: utf-8 -*-
+import os
+import pickle
+import numpy as np
+from math import sqrt, log, erf
+
+def brownian(N):
     
-    m = pd.merge(p1, p2, left_on=pk[0], right_on=pk[1], how=how)
+    b = np.insert(  np.random.normal(0., 1., int(N))*np.sqrt(1./N),  0, 0)
+    return np.cumsum(b), b
+
+def GBM(So, mu, sigma, W, T):
     
-    res = m[pk[0]]
+    N = W.shape[0]
+    t = np.linspace(0.,T,N)
+    S = []
     
-    for col1, col2 in zip(flt_col[0],flt_col[1]):
-        
-        if col1 == col2: col1, col2 = col1 + "_x", col2 + "_y"
-        
-        res = pd.concat([res, m[[col1,col2]], pd.Series(m[col1]-m[col2], name=col1+"_"+col2)], axis=1)
-    
-    for col1, col2 in zip(str_col[0],str_col[1]):
-        
-        if col1 == col2: col1, col2 = col1 + "_x", col2 + "_y"
-        
-        res = pd.concat([res, m[[col1,col2]], pd.Series(m[col1]==m[col2], name=col1+"_"+col2)], axis=1)
-        
+    for i in range(0,int(N)):
+        S.append(  So*np.exp((mu - 0.5 * sigma**2) * t[i] + sigma * W[i])  )
+    return S, t
+
+def CLF(S, m, v, N): return (m + S)/(v*sqrt(N))
+
+def cnd(x): return (1.0 + erf(x / sqrt(2.0))) / 2.0
+
+def getdata(path):
+    os.path.exists(path)
+    fichier = open(path,"rb")
+    tmpPickle = pickle.Unpickler(fichier)
+    res = tmpPickle.load()
+    fichier.close()
     return res
+
+def setdata(path, donnees):
+    fichier = open(path,"wb")
+    tmpPickle = pickle.Pickler(fichier)
+    tmpPickle.dump(donnees)
+    fichier.close()
