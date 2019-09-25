9f613744b471107fca696bda790e55f7d0f2b658
Jonathan <jonathan.vengadasalam@gmail.com>
2019-09-25 14:41:45 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,45 +3,90 @@ from utils import *
 from manager.functions import *
 from artificial_intelligence.ai import AI
 from environnements.envConnect import Connect
-from neural_network_models.modelBuilder import model
 from tensorflow.python.keras.models import load_model
+from neural_network_models.modelBuilder import build_model_for_connect_game
 
-def pretraining_for_connect_game():
+def pretraining_for_connect_game(iteration=2000):
 
+    #initialize object and set parameters
+    x, y, l, e = [], [], [], []
+    rootenv = Connect()
     a1 = AI(itermax=50,alpha=0.5,use_subtree=True)
     a2 = AI(itermax=100,alpha=0.5,use_subtree=True)
     a3 = AI(itermax=150,alpha=0.5,use_subtree=True)
     a4 = AI(itermax=225,alpha=0.5,use_subtree=True)
     a5 = AI(itermax=300,alpha=0.5,use_subtree=True)
-
-    x, y, l, e = [], [], [], []
+    config = { "filters":128, "kernel_size":2, "strides":(1,1), "padding":"same", "kernel_regularizer":l2(1e-4)}
+    mod = build_model_for_connect_game(name="pretrained",nb_resnet=8,config)
     
-    rootenv = Connect()
-    
-    selfplay(rootenv,2000,a1,a1,x,y,l,e)
-    selfplay(rootenv,2000,a1,a2,x,y,l,e)
-    selfplay(rootenv,2000,a1,a3,x,y,l,e)
-    selfplay(rootenv,2000,a1,a4,x,y,l,e)
-    selfplay(rootenv,2000,a1,a5,x,y,l,e)
-    selfplay(rootenv,2000,a2,a2,x,y,l,e)
-    selfplay(rootenv,2000,a2,a3,x,y,l,e)
-    selfplay(rootenv,2000,a2,a4,x,y,l,e)
-    selfplay(rootenv,2000,a2,a5,x,y,l,e)
-    selfplay(rootenv,2000,a3,a3,x,y,l,e)
-    selfplay(rootenv,2000,a3,a4,x,y,l,e)
-    selfplay(rootenv,2000,a3,a5,x,y,l,e)
-    selfplay(rootenv,2000,a4,a4,x,y,l,e)
-    selfplay(rootenv,2000,a4,a5,x,y,l,e)
-    selfplay(rootenv,2000,a5,a5,x,y,l,e)
+    #play game to collect data
+    selfplay(rootenv,iteration,a1,a1,x,y,l,e)
+    selfplay(rootenv,iteration,a1,a2,x,y,l,e)
+    selfplay(rootenv,iteration,a1,a3,x,y,l,e)
+    selfplay(rootenv,iteration,a1,a4,x,y,l,e)
+    selfplay(rootenv,iteration,a1,a5,x,y,l,e)
+    selfplay(rootenv,iteration,a2,a2,x,y,l,e)
+    selfplay(rootenv,iteration,a2,a3,x,y,l,e)
+    selfplay(rootenv,iteration,a2,a4,x,y,l,e)
+    selfplay(rootenv,iteration,a2,a5,x,y,l,e)
+    selfplay(rootenv,iteration,a3,a3,x,y,l,e)
+    selfplay(rootenv,iteration,a3,a4,x,y,l,e)
+    selfplay(rootenv,iteration,a3,a5,x,y,l,e)
+    selfplay(rootenv,iteration,a4,a4,x,y,l,e)
+    selfplay(rootenv,iteration,a4,a5,x,y,l,e)
+    selfplay(rootenv,iteration,a5,a5,x,y,l,e)
 
+    #convert and save data
     nx = np.array(x)
-    py = np.array(y)[:,:-1]
     vy = np.array(y)[:,-1]
-
-    setdata("data/nx",nx)
-    setdata("data/py",py)
-    setdata("data/vy",vy)
+    py = np.array(y)[:,:-1]
+    setdata("data/pretrained/nx",nx)
+    setdata("data/pretrained/py",py)
+    setdata("data/pretrained/vy",vy)
     
-    mod = model("master")
+    #train model and save
     mod.fit(nx, [py,vy], 64, 2, validation_split=0.2)
     mod.save("neural_network_models/pretrained")
+
+    return mod
+
+def selftraining_for_connect(name, iteration=4000):
+
+    #initialize object and set parameters
+    x, y, l, e = [], [], [], []
+    rootenv = Connect()
+    mod = load_model("neural_network_models/" + name)
+    a = AI(itermax=300,alpha=0.3,name=name,model=mod,formula="UCB1",use_subtree=True,stochastic=True,temperature=0.3)
+
+    #play against himself and collect data
+    selfplay(rootenv,iteration,a,a,x,y,l,e)
+
+    #convert and save data
+    nx = np.array(x)
+    vy = np.array(y)[:,-1]
+    py = np.array(y)[:,:-1]
+    setdata("data/pretrained/" + name + "/nx",nx)
+    setdata("data/pretrained/" + name + "/py",py)
+    setdata("data/pretrained/" + name + "/vy",vy)
+
+    #train model and save
+    mod.fit(nx, [py,vy], 64, 2, validation_split=0.2)
+    mod.save("neural_network_models/" + name)
+
+    return mod
+
+def evaluate_network(name1, name2, iteration=200, alpha=0.05):
+
+    #initialize object and set parameters
+    x, y, l, e = [], [], [], []
+    rootenv = Connect()
+    mod1 = load_model("neural_network_models/" + name1)
+    mod2 = load_model("neural_network_models/" + name2)
+    a1 = AI(itermax=300,alpha=0.3,name=name1,model=mod,formula="UCB1",use_subtree=True,stochastic=True,temperature=0.3)
+    a2 = AI(itermax=300,alpha=0.3,name=name2,model=mod,formula="UCB1",use_subtree=True,stochastic=True,temperature=0.3)
+
+    #play game and collect results
+    selfplay(rootenv,iteration,a1,a2,x,y,l,e)
+
+    #test mod2
+    return z_test(x=l,m=0,alpha=alpha,unilateral=True)
