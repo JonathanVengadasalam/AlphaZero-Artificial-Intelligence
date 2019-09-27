1358a40cef2313fab709a561c724fadc4708f628
Jonathan <jonathan.vengadasalam@gmail.com>
2019-09-27 02:03:47 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from utils import *
 from manager.functions import *
 from artificial_intelligence.ai import AI
-from environnements.envConnect import Connect
+from environnements import envConnect as env
 from tensorflow.python.keras.models import load_model
 from neural_network_models.modelBuilder import build_model_for_connect_game
 
@@ -10,14 +10,13 @@ def pretraining_for_connect_game(iteration=2000):
 
     #initialize object and set parameters
     x, y, l, e = [], [], [], []
-    rootenv = Connect()
+    rootenv = env.Connect()
     a1 = AI(itermax=50,alpha=0.5,use_subtree=True)
     a2 = AI(itermax=100,alpha=0.5,use_subtree=True)
     a3 = AI(itermax=150,alpha=0.5,use_subtree=True)
     a4 = AI(itermax=225,alpha=0.5,use_subtree=True)
     a5 = AI(itermax=300,alpha=0.5,use_subtree=True)
-    config = { "filters":128, "kernel_size":2, "strides":(1,1), "padding":"same", "kernel_regularizer":l2(1e-4)}
-    mod = build_model_for_connect_game(name="pretrained",nb_resnet=8,**config)
+    mod = build_model_for_connect_game(name="pretrained", env.Height, env.Width, env.Depth, nb_resnet=8)
     
     #play game to collect data
     selfplay(rootenv,iteration,a1,a1,x,y,l,e)
@@ -54,7 +53,7 @@ def selftraining_for_connect(name, iteration=4000):
 
     #initialize object and set parameters
     x, y, l, e = [], [], [], []
-    rootenv = Connect()
+    rootenv = env.Connect()
     mod = load_model("neural_network_models/" + name)
     a = AI(itermax=300,alpha=0.3,name=name,model=mod,formula="UCB1",use_subtree=True,stochastic=True,temperature=0.3)
 
@@ -79,7 +78,7 @@ def evaluate_network(name1, name2, iteration=200, alpha=0.05):
 
     #initialize object and set parameters
     x, y, l, e = [], [], [], []
-    rootenv = Connect()
+    rootenv = env.Connect()
     mod1 = load_model("neural_network_models/" + name1)
     mod2 = load_model("neural_network_models/" + name2)
     a1 = AI(itermax=300,alpha=0.3,name=name1,model=mod,formula="UCB1",use_subtree=True,stochastic=True,temperature=0.3)
